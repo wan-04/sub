6e843dc013c4df35e861b960f0c6625bd9378223
darklmao <lmaodark>
2025-07-26 14:03:13 +0700

    update

--- a.sh
+++ a.sh
@@ -1,5 +1,5 @@
 wget "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
-tar -xf https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok config add-authtoken 2SPhhGuL5yc48a51XVukQbTiZ0j_rGyiNEkpkjH7GwXpvXAJ
 
 ngrok tcp 8022
